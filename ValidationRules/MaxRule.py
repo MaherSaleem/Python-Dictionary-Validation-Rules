f569e369a4865d7fdc6022bdb1c4b94bd43a6af8
Maher Khdeir <maher.khdeir@gmail.com>
2019-04-19 17:58:26 +0300

    Fix In Max Rule

--- ValidationRules/MaxRule.py
+++ ValidationRules/MaxRule.py
@@ -2,12 +2,12 @@ from ValidationRule import ValidationRule
 
 
 class MaxRule(ValidationRule):
-    def __init__(self, attributeName, attributeData, args):
+    def __init__(self, attributeName, attributeData, args=[]):
         super().__init__(attributeName, attributeData, args)
         self._args = list(map(int, self._args))
 
     def isValid(self):
-        return self.get_attributeData() <= self.get_attributeData()
+        return self.get_attributeData() <= self.maxValue
 
     def getValidationDefaultMessage(self):
         return "The %s field must at max %s" % (self.get_attributeName(), self.maxValue)
