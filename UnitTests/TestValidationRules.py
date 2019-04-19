bf58c2d374d2c7d9e63e26cd1e550c98501cae05
Maher Khdeir <maher.khdeir@gmail.com>
2019-04-19 17:58:54 +0300

    Added Unit Tests for the project

---
+++ UnitTests/TestValidationRules.py
@@ -0,0 +1,34 @@
+import unittest
+from ValidationRules.StringRule import StringRule
+from ValidationRules.MaxRule import MaxRule
+from ValidationRules.EmailRule import EmailRule
+
+
+class TestValidationRules(unittest.TestCase):
+    def testStringRuleValidCase(self):
+        rule = StringRule('stringAttribute', 'This is a string')
+        self.assertEqual(rule.isValid(), True)
+
+    def testStringRuleInValidCase(self):
+        rule = StringRule('stringAttribute', 12346)
+        self.assertEqual(rule.isValid(), False)
+
+    def testMaxRuleValidCase(self):
+        rule = MaxRule('MaxRule', 20, ['25'])
+        self.assertEqual(rule.isValid(), True)
+
+    def testMaxRuleInValidCase(self):
+        rule = MaxRule('MaxRule', 20, ['15'])
+        self.assertEqual(rule.isValid(), False)
+
+    def testEmailRuleValidCase(self):
+        rule = EmailRule('EmailRule', 'maher@birzeit.edu')
+        self.assertEqual(rule.isValid(), True)
+
+    def testEmailRuleInValidCase(self):
+        rule = EmailRule('EmailRule', 'maherbirzeit.edu')
+        self.assertEqual(rule.isValid(), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
